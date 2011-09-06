66a9297bf0ade46eb51449eb1c10522d0868c80f
Gareth Lloyd <glloyd@gmail.com>
2011-09-06 09:22:25 +0100

    fix oauth problems and modify protocol

--- twister/protocol.py
+++ twister/protocol.py
@@ -1,200 +1,33 @@
-# -*- test-case-name: twittytwister.test.test_streaming -*-
-#
-# Copyright (c) 2010 Ralph Meijer <ralphm@ik.nu>
-# See LICENSE.txt for details
-
-"""
-Twitter Streaming API.
-
-@see: U{http://dev.twitter.com/pages/streaming_api}.
-"""
-
-import simplejson as json
-
+import json
 from twisted.internet import defer
-from twisted.protocols.basic import LineReceiver
+from twisted.protocols.basic import LineOnlyReceiver
 from twisted.protocols.policies import TimeoutMixin
 from twisted.python import log
 from twisted.web.client import ResponseDone
 from twisted.web.http import PotentialDataLoss
 
-class LengthDelimitedStream(LineReceiver):
-    """
-    Length-delimited datagram decoder protocol.
-
-    Datagrams are prefixed by a line with a decimal length in ASCII. Lines are
-    delimited by C{\r\n} and maybe empty, for keep-alive purposes.
-    """
-
-    def __init__(self):
-        self._buffer = None
-        self._bufferLength = None
-        self._expectedLength = None
-
-
-    def lineReceived(self, line):
-        """
-        Called when a line is received.
-
-        We expect a length in bytes or an empty line for keep-alive. If
-        we got a length, switch to raw mode to receive that amount of bytes.
-        """
-        if line and line.isdigit():
-            self._expectedLength = int(line)
-            self._buffer = []
-            self._bufferLength = 0
-            self.setRawMode()
-        else:
-            self.keepAliveReceived()
-
-
-    def rawDataReceived(self, data):
-        """
-        Called when raw data is received.
-
-        Fill the raw buffer C{_rawBuffer} until we have received at least
-        C{_lenghtExpected} bytes. Call objectReceived with the received byte
-        string of the expected size. Then switch back to line mode with the
-        remainder of the buffer.
-        """
-        self._buffer.append(data)
-        self._bufferLength += len(data)
-
-        if self._bufferLength >= self._expectedLength:
-            receivedData = ''.join(self._buffer)
-            expectedData = receivedData[:self._expectedLength]
-            extraData = receivedData[self._expectedLength:]
-
-            self._buffer = None
-            self._bufferLength = None
-            self._expectedLength = None
-
-            self.datagramReceived(expectedData)
-            self.setLineMode(extraData)
-
-
-    def datagramReceived(self, data):
-        """
-        Called when a datagram is received.
-        """
-        raise NotImplementedError()
-
-
-    def keepAliveReceived(self):
-        """
-        Called when a empty line as keep-alive is received.
-
-        This can be overridden for logging purposes.
-        """
-
-
-
-class TwitterObject(object):
-    """
-    A Twitter object.
-    """
-    raw = None
-
-    @classmethod
-    def fromDict(cls, data):
-        """
-        Fill this objects attributes from a dict for known properties.
-        """
-        obj = cls()
-        obj.raw = data
-        for name, value in data.iteritems():
-            if name in cls.SIMPLE_PROPS:
-                setattr(obj, name, value)
-            elif name in cls.COMPLEX_PROPS:
-                value = cls.COMPLEX_PROPS[name].fromDict(value)
-                setattr(obj, name, value)
-
-        return obj
-
-
-
-class Status(TwitterObject):
-    """
-    Twitter Status.
-    """
-    SIMPLE_PROPS = set(['created_at', 'id', 'text', 'source', 'truncated',
-        'in_reply_to_status_id', 'in_reply_to_screen_name',
-        'in_reply_to_user_id', 'favorited', 'user_id', 'geo'])
-    COMPLEX_PROPS = {}
-
-# circular reference:
-Status.COMPLEX_PROPS['retweeted_status'] = Status
-
-
-
-class User(TwitterObject):
-    """
-    Twitter User.
-    """
-    SIMPLE_PROPS = set(['id', 'name', 'screen_name', 'location', 'description',
-        'profile_image_url', 'url', 'protected', 'followers_count',
-        'profile_background_color', 'profile_text_color', 'profile_link_color',
-        'profile_sidebar_fill_color', 'profile_sidebar_border_color',
-        'friends_count', 'created_at', 'favourites_count', 'utc_offset',
-        'time_zone', 'following', 'notifications', 'statuses_count',
-        'profile_background_image_url', 'profile_background_tile', 'verified',
-        'geo_enabled'])
-    COMPLEX_PROPS = {'status': Status}
-
-# circular reference:
-Status.COMPLEX_PROPS['user'] = User
-
-
-
-class TwitterStream(LengthDelimitedStream, TimeoutMixin):
-    """
-    Twitter Stream.
-
-    This protocol decodes an JSON encoded stream of Twitter statuses and
-    associated datastructures, where each datagram is length-delimited.
-
-    L{TimeoutMixin} is used to disconnect the stream in case Twitter stops
-    sending data, including the keep-alives that usually result in traffic
-    at least every 30 seconds. If not passed using C{timeoutPeriod}, the
-    timeout period is set to 60 seconds.
-    """
-
-    def __init__(self, callback, timeoutPeriod=60):
-        LengthDelimitedStream.__init__(self)
-        self.setTimeout(timeoutPeriod)
+class TwitterStreamingProtocol(LineOnlyReceiver, TimeoutMixin):
+    def __init__(self, callback, timeout_seconds=60):
+        self.setTimeout(timeout_seconds)
         self.callback = callback
         self.deferred = defer.Deferred()
 
-
-    def dataReceived(self, data):
-        """
-        Called when data is received.
-
-        This overrides the default implementation from LineReceiver to
-        reset the connection timeout.
-        """
-        self.resetTimeout()
-        LengthDelimitedStream.dataReceived(self, data)
-
-
-    def datagramReceived(self, data):
+    def lineReceived(self, line):
         """
         Decode the JSON-encoded datagram and call the callback.
         """
-        try:
-            obj = json.loads(data)
-        except ValueError, e:
-            log.err(e, 'Invalid JSON in stream: %r' % data)
-            return
+        line = line.strip()
+        if line:
+            try:
+                obj = json.loads(line)
+            except ValueError, e:
+                log.err(e, 'Invalid JSON in stream: %r' % line)
+                return
+            self.callback(obj)
 
-        if u'text' in obj:
-            obj = Status.fromDict(obj)
-        else:
-            log.msg('Unsupported object %r' % obj)
-            return
-
-        self.callback(obj)
-
+    def dataReceived(self, data):
+        self.resetTimeout()
+        LineOnlyReceiver.dataReceived(self, data)
 
     def connectionLost(self, reason):
         """
@@ -205,12 +38,13 @@ class TwitterStream(LengthDelimitedStream, TimeoutMixin):
         stream, instead of L{ResponseDone}. Other exceptions are treated
         as error conditions.
         """
+        print 'connection lost'
+        print self.deferred
         self.setTimeout(None)
         if reason.check(ResponseDone, PotentialDataLoss):
             self.deferred.callback(None)
         else:
             self.deferred.errback(reason)
-
 
     def timeoutConnection(self):
         """
@@ -222,3 +56,4 @@ class TwitterStream(LengthDelimitedStream, TimeoutMixin):
         closed when called.
         """
         self.transport.stopProducing()
+        self.deferred.errback(None)
--- twister/stream.py
+++ twister/stream.py
@@ -1,18 +1,17 @@
-import time
 from urllib import quote
 
 from twisted.internet import defer, reactor
-from twisted.web import client, error, http_headers
+from twisted.web import client, error
 from twisted.web.http_headers import Headers
-import oauth2
+from oauth import oauth
 
 import protocol
 
-STREAM_URL = 'http://stream.twitter.com/1/statuses/%s.json'
+STREAM_URL = 'https://stream.twitter.com/1/statuses/%s.json'
 
 class Stream(object):
 
-    protocol = protocol.TwitterStream
+    protocol = protocol.TwitterStreamingProtocol
 
     def __init__(self, consumer=None, token=None, timeout=0):
         self.timeout = timeout
@@ -20,47 +19,46 @@ class Stream(object):
         self.token = token
         self.agent = client.Agent(reactor)
 
-    def _make_auth_headers(self, http_method, url, parameters={}, headers={}):
-        oauth_request = oauth2.Request.from_consumer_and_token(self.consumer, is_form_encoded=True,
+    def _make_oauth1_headers(self, http_method, url, parameters={}, headers={}):
+        oauth_request = oauth.OAuthRequest.from_consumer_and_token(self.consumer,
             token=self.token, http_method=http_method, http_url=url, parameters=parameters)
-        oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), self.consumer, self.token)
+        oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), self.consumer, self.token)
 
         headers.update(oauth_request.to_header())
-        for key, value in headers.items():
-            headers[key] = [value]
         return headers
 
-    def _stream(self, api_call, http_method, status_receiver, args):
+    def _connect(self, api_call, http_method, status_receiver, args):
         def get_initial_response(response):
             if response.code == 200:
                 protocol = self.protocol(status_receiver)
                 response.deliverBody(protocol)
                 return protocol
             else:
-                response.deliverBody(PrintProtocol())
                 raise error.Error(response.code, response.phrase)
 
         url = STREAM_URL % api_call
+        raw_headers = {}
 
         args = args or {}
-        args['delimited'] = 'length'
         arg_str = _urlencode(args)
-        print arg_str
         if http_method == 'GET':
             url += '?' + arg_str
             body_producer = None
         else:
+            raw_headers['Content-Type'] = 'application/x-www-form-urlencoded'
             body_producer = StringProducer(arg_str)
-        auth_headers = self._make_auth_headers(http_method, url, {})
-        print auth_headers
-        headers = Headers(auth_headers)
-        print 'Fetching', url
+        auth_headers = self._make_oauth1_headers(http_method, url, args, raw_headers)
+        raw_headers = dict([(name, [value])
+                           for name, value
+                           in auth_headers.iteritems()])
+        headers = Headers(raw_headers)
+        print http_method, url
         d = self.agent.request(http_method, url, headers, body_producer)
         d.addCallback(get_initial_response)
         return d
 
     def filter(self, status_receiver, args=None):
-        return self._stream('filter', 'GET', status_receiver, args)
+        return self._connect('filter', 'POST', status_receiver, args)
 
     def follow(self, status_receiver, follow):
         return self.filter(status_receiver, {'follow': ','.join(follow)})
@@ -70,8 +68,8 @@ class Stream(object):
 
 def _urlencode(headers):
     encoded = []
-    for key,value in headers.iteritems():
-        encoded.append('%s=%s' %
+    for key, value in headers.iteritems():
+        encoded.append("%s=%s" %
             (quote(key.encode("utf-8")),
             quote(value.encode("utf-8"))))
     return '&'.join(encoded)
@@ -90,20 +88,5 @@ class StringProducer(object):
         pass
 
     def stopProducing(self):
-        pass
-
-class PrintProtocol(object):
-    def __init__(self):
-        self.f = open('log', 'w')
-
-    def dataReceived(self, data):
-        f.write(data)
-    def connectionLost(self, reason):
-        pass
-
-    def makeConnection(self, transport):
-        pass
-
-    def connectionMade(self):
         pass
 
