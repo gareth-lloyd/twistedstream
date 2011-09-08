076b6c0149c8a9560c4d7da5daac2765d5f5f547
Gareth Lloyd <glloyd@gmail.com>
2011-09-08 12:10:29 +0100

    add state tests

--- tests/state.py
+++ tests/state.py
@@ -1,21 +1,41 @@
 import unittest
-from datetime import datetime
 from twistedstream import Stream
 from twistedstream.stream import (DISCONNECTED, CONNECTING, CONNECTED, 
         BACKING_OFF)
-from mock import Mock
-from oauth import oauth
-
 
 class StateTest(unittest.TestCase):
-
+    """with four states, there are 16 possible transitions, including
+    'transitioning' to the same state. 
+    """
     def setUp(self):
-        self.stream = Stream(Mock(spec=oauth.OAuthConsumer),
-            Mock(spec=oauth.OAuthToken))
+        self.stream = Stream(None, None)
 
-    def test_disconnected_to_connecting(self):
-        self.stream._advance_state_to(CONNECTING)
+    def test_disconnected(self):
+        "can go from disconnected to connecting or disconnected"
+        self._valid(DISCONNECTED, (CONNECTING, DISCONNECTED))
+        self._invalid(DISCONNECTED, (CONNECTED, BACKING_OFF))
 
-    def test_disconnected_to_connected(self):
-        "Can't transition to CONNECTED without passing through CONNECTING"
-        self.assertRaises(ValueError, self.stream._advance_state_to, CONNECTED)
+    def test_connected(self):
+        "from connected can transition to disconnected only"
+        self._valid(CONNECTED, (DISCONNECTED, ))
+        self._invalid(CONNECTED, (CONNECTED, CONNECTING, BACKING_OFF))
+
+    def test_connecting(self):
+        "from connecting can transition to disconnected, connected or backing off"
+        self._valid(CONNECTING, (DISCONNECTED, CONNECTED, BACKING_OFF))
+        self._invalid(CONNECTING, (CONNECTING, ))
+
+    def test_backing_off(self):
+        "from backing off, can transition to disconnected, or connecting"
+        self._valid(BACKING_OFF, (DISCONNECTED, CONNECTING))
+        self._invalid(BACKING_OFF, (BACKING_OFF, CONNECTED))
+
+    def _invalid(self, initial_state, invalid_states):
+        for invalid_state in invalid_states:
+            self.stream.state = initial_state
+            self.assertRaises(ValueError, self.stream._advance_state_to, invalid_state)
+
+    def _valid(self, initial_state, valid_states):
+        for valid_state in valid_states:
+            self.stream.state = initial_state
+            self.stream._advance_state_to(valid_state)
